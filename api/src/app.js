e0966dde298feab45b1c7b9e2a9c0e328ec22d10
Camilop4 <jcrp_2005@hotmail.com>
2025-12-21 10:47:28 -0500

    fixed erro CORS in vote

--- api/src/app.js
+++ api/src/app.js
@@ -56,7 +56,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
