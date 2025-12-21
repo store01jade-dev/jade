e4f4447192e6f0691d009f80e0541cb3808ab0b9
Camilop4 <jcrp_2005@hotmail.com>
2025-12-21 03:49:17 -0500

    fixed url for cloudinary

--- api/src/cloudinaryConfig/index.js
+++ api/src/cloudinaryConfig/index.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({ 
-    cloudinary_url: process.env.CLOUDINARY_URL
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:    process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
